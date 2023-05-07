86038f7881966dafcc00f10b4f32618d2829472b
Tanvir <tanvir.bd.global@gmail.com>
2023-05-07 15:29:48 +0600

    added admin login api

--- src/api/controllers/adminCtrl.js
+++ src/api/controllers/adminCtrl.js
@@ -8,7 +8,7 @@ let adminCtrl = {}
 //API : /admin/register
 //Method : POST
 //Access : access with admin approval
-//Description : reg now admin
+//Description : reg admin api
 adminCtrl.rgeAdmin = async (req, res) => {
 
     const bodyObject = Object.keys(req.body);
@@ -47,7 +47,53 @@ adminCtrl.rgeAdmin = async (req, res) => {
 }
 
 
+//API : /admin/login
+//Method : Get
+//Access : no access needed
+//Description : login admin admin
+adminCtrl.loginAdmin = async (req, res) => {
+    const { email, password } = req?.body || {}
 
+    if (!email || !password) {
+        return res.status(400).json({
+            "success": false,
+            "message": "Invalid input!"
+        });
+    }
+
+
+    try {
+        //find email by admin
+        const adminData = await Admin.findOne({ email })
+        const { email: adminEmail, password: adminPasswordHash } = adminData || {}
+
+        //check user password valid or not
+        const checkPassword = await bcrypt.compare(password, adminPasswordHash)
+
+        //admin login
+        if (checkPassword) {
+            //admin login successfully
+            return res.status(200).json({
+                "success": true,
+                "message": "Login Successful",
+                "accessToken": "4566223225332222"
+            })
+        } else {
+            //admin login error
+            return res.status(401).json({
+                "success": false,
+                "message": "Wrong password!"
+            });
+        }
+
+    } catch (error) {
+        console.log("find email by admin", error.message);
+        return res.status(500).json({
+            "success": false,
+            "message": "internal server error!"
+        });
+    }
+}
 
 
 
--- src/api/routes/adminsRoute.js
+++ src/api/routes/adminsRoute.js
@@ -4,5 +4,6 @@ import adminCtrl from '../controllers/adminCtrl.js';
 const router = express.Router();
 
 router.route('/register').post(adminCtrl.rgeAdmin);
+router.route('/login').get(adminCtrl.loginAdmin);
 
 export default router;
