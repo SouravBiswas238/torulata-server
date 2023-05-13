e1b6a249a0baa8a883ce694918b51a408f4454df
Sumon Bala <Sumonche016@gmail.com>
2023-05-13 23:42:34 +0600

    conflict solve

--- src/api/routes/ProductsRoute.js
+++ src/api/routes/ProductsRoute.js
@@ -8,10 +8,6 @@ const productCtrl = new ProductCtrl();
 router.route('/addProduct').post(productCtrl.addProduct);
 router.route('/deleteProduct/:productId').delete(productCtrl.deleteProducts);
 router.route('/fetchProducts').get(productCtrl.getProducts);
-<<<<<<< HEAD
 router.route('/singleProduct/:productId').get(productCtrl.getSingleProducts);
-=======
-router.route('/singleProduct/:id').get(productCtrl.getSingleProducts);
->>>>>>> 28a8fcd9208578bd89b1f5fee0d7d401d7c1db89
 
 export default router;
