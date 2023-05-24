203498f3fa3ba205d1a25da5d8f7170118e4edac
Tanvir <tanvir.bd.global@gmail.com>
2023-05-25 01:31:51 +0600

    removed some pakages

--- src/config/properties.js
+++ src/config/properties.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const properties = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 5000,
 
     MONGO_URI: process.env.MONGO_URI || `mongodb+srv://torulota:Jl4N2Nyy2OZzNCPA@cluster0.ds6trld.mongodb.net/test`,
     SERVER_URL: process.env.SERVER_URL || ``
