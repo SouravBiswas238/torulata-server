4fc94a9eeaa0181b4c10be5f8bb99a52a2b9e6e7
Tanvir <tanvir.bd.global@gmail.com>
2023-05-28 13:21:54 +0600

    added add parmid and remove api

--- src/api/routes/adminsRoute.js
+++ src/api/routes/adminsRoute.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.route('/login').patch(adminCtrl.loginAdmin);
 router.route('/register').post(adminCtrl.rgeAdmin);
-router.route('/remove').delete(jwtVerify, adminCtrl.removeAdmin);
+router.route('/all-admin').get(adminCtrl.getAllAdmin);
+router.route('/remove').delete(adminCtrl.removeAdmin);
 router.route('/email-verify/:hash').patch(adminCtrl.verifyEmail);
 router.route('/password-update').patch(adminCtrl.passwordUpdate);
 router.route('/give-admin-access').patch(adminCtrl.giveAdminAccess);
