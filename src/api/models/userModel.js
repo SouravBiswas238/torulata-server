db5bddd5d48316ecb5532e9baf8ae4d0ee6ac48a
SouravBiswas238 <souravbiswas427@gmail.com>
2023-06-18 07:52:24 +0600

    adding user collection

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
 
-    user_id: {
-        type: String,
-        required: true
-    },
+    user_id: { type: String, required: true, unique: true },
     user_name: {
         type: String,
 
